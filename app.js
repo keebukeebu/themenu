dcd4450c3f3fb7d4479e62bc98f9b07f5bcb1c0a
hakeeb <akibibrahim@yahoo.com>
2023-02-20 22:26:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const icon = document.querySelector('.icon');
+const nav = document.querySelector('nav'); 
+
+icon.addEventListener('click',()=>{
+  icon.classList.toggle('close');
+  nav.classList.toggle('show');
+
+
+}) 
